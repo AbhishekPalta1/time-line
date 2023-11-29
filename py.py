85a9058b7dea214273362a40a44aa9af5fb97b53
AbhishekPalta1 <151897720+AbhishekPalta1@users.noreply.github.com>
2023-11-29 21:34:22 +0530

    Add files via upload

---
+++ py.py
@@ -0,0 +1,13 @@
+import time
+
+while (True):
+    
+    timestamp = time.strftime('%H:%M:%S')
+    print(timestamp)
+    time.sleep(5)
+timestamp = time.strftime("%H")
+print(timestamp)
+timestamp = time.strftime("%M")
+print(timestamp)
+timestamp = time.strftime("%S")
+print(timestamp)
